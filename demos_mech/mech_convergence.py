e004775ccc26044835b3a38c1b37c7cbd34609c8
Alexander Hatle <42936072+aledha@users.noreply.github.com>
2025-02-06 19:35:55 -0600

    Used formatter

--- demos_mech/mech_convergence.py
+++ demos_mech/mech_convergence.py
@@ -4,10 +4,12 @@ from dolfinx import io, fem
 from mpi4py import MPI
 import ufl
 import sys
-sys.path.append('../')
+
+sys.path.append("../")
 
 from src.hyperelasticity import HyperelasticProblem
 from src.utils import interpolate
+
 
 def solve(h, lagrange_order=2, save_solution=False):
     problem = HyperelasticProblem(h, lagrange_order)
@@ -15,13 +17,16 @@ def solve(h, lagrange_order=2, save_solution=False):
 
     def left(x):
         return np.isclose(x[0], 0)
+
     def right(x):
         return np.isclose(x[0], 1)
 
-    problem.boundary_conditions([left, right], vals=[0.0, 0], bc_types=['d1', 'n'])
+    problem.boundary_conditions([left, right], vals=[0.0, 0], bc_types=["d1", "n"])
     problem.setup_solver()
+
     def tension(x):
-        return 5 * np.exp(-5*((x[1]-0.5)**2 + (x[2]-0.5)**2))
+        return 5 * np.exp(-5 * ((x[1] - 0.5) ** 2 + (x[2] - 0.5) ** 2))
+
     problem.set_tension(tension)
 
     if save_solution:
@@ -35,36 +40,39 @@ def solve(h, lagrange_order=2, save_solution=False):
         vtx.close()
     return problem.u
 
-def L2_diff_coarse_fine(h, u_fine, lagrange_order = 2):
+
+def L2_diff_coarse_fine(h, u_fine, lagrange_order=2):
     # Interpolate the coarser solution into finer mesh and compute error there?
     # Opposite would lose details from the fine solution.
     u_coarse = solve(h, lagrange_order)
 
-    u_coarse_interp = interpolate(u_from = u_coarse, V_to = u_fine.function_space)
+    u_coarse_interp = interpolate(u_from=u_coarse, V_to=u_fine.function_space)
 
     comm = u_fine.function_space.mesh.comm
-    error = fem.form((u_fine - u_coarse_interp) ** 2 * ufl.dx) 
+    error = fem.form((u_fine - u_coarse_interp) ** 2 * ufl.dx)
     E = np.sqrt(comm.allreduce(fem.assemble_scalar(error), MPI.SUM))
     return E
 
-def convergence_plot(h_fine, hs, plot_title, lagrange_order = 2):
+
+def convergence_plot(h_fine, hs, plot_title, lagrange_order=2):
     u_fine = solve(h_fine)
     errors = np.zeros_like(hs)
     for i in range(len(hs)):
-        errors[i] = L2_diff_coarse_fine(hs[i], 
-                                        u_fine, 
-                                        lagrange_order = lagrange_order)
+        errors[i] = L2_diff_coarse_fine(hs[i], u_fine, lagrange_order=lagrange_order)
 
-    order = (np.log(errors[-1]) - np.log(errors[-2])) / (np.log(hs[-1]) - np.log(hs[-2]))
-    
-    fig, ax = plt.subplots(figsize=(8,5))
-    ax.loglog(hs, errors, '-o', label = "order = {:.3f}".format(order))
-    ax.set_xlabel(r'$h$')
-    ax.set_ylabel('Error')
-    ax.set_title(f'Convergence plot of hyperelastic problem')
+    order = (np.log(errors[-1]) - np.log(errors[-2])) / (
+        np.log(hs[-1]) - np.log(hs[-2])
+    )
+
+    fig, ax = plt.subplots(figsize=(8, 5))
+    ax.loglog(hs, errors, "-o", label="order = {:.3f}".format(order))
+    ax.set_xlabel(r"$h$")
+    ax.set_ylabel("Error")
+    ax.set_title(f"Convergence plot of hyperelastic problem")
     ax.legend()
-    fig.savefig(plot_title, bbox_inches='tight')
+    fig.savefig(plot_title, bbox_inches="tight")
     fig.show()
+
 
 h_fine = 0.075
 hs = [0.1, 0.15, 0.2, 0.3, 0.4]
--- src/coupled_model.py
+++ src/coupled_model.py
@@ -13,6 +13,7 @@ import adios4dolfinx
 
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
+
 def translateODE(odeFileName, schemes):
     odeFolder = str(Path.cwd().parent) + "/odes/"
     model_path = Path(odeFolder + odeFileName + ".py")
@@ -23,7 +24,9 @@ def translateODE(odeFileName, schemes):
     else:
         print("ODE already translated")
 
-#translateODE('tentusscher_land_1way', [gotranx.schemes.Scheme.generalized_rush_larsen])
+
+# translateODE('tentusscher_land_1way', [gotranx.schemes.Scheme.generalized_rush_larsen])
+
 
 @dataclass
 class WeaklyCoupledModel:
@@ -42,17 +45,19 @@ class WeaklyCoupledModel:
             xdmf.write_mesh(self.ep.domain)
 
     def _save_Ta(self):
-        Ta_array = self.ep.ode.model.monitor_values(self.t.value, 
-                                              self.ep.ode.states, 
-                                              self.ep.ode.params)[self.Ta_index]
+        Ta_array = self.ep.ode.model.monitor_values(
+            self.t.value, self.ep.ode.states, self.ep.ode.params
+        )[self.Ta_index]
         self.Ta_lagrange.x.array[:] = Ta_array
         self.Ta.interpolate(self.Ta_lagrange)
 
-        adios4dolfinx.write_function_on_input_mesh("Ta.bp",
-                                                   self.Ta,
-                                                   mode=adios4dolfinx.adios2_helpers.adios2.Mode.Write,
-                                                   time=self.t.value,
-                                                   name="Ta")
+        adios4dolfinx.write_function_on_input_mesh(
+            "Ta.bp",
+            self.Ta,
+            mode=adios4dolfinx.adios2_helpers.adios2.Mode.Write,
+            time=self.t.value,
+            name="Ta",
+        )
 
     def solve_ep_save_Ta(self, T):
         while self.t.value < T + self.dt:
@@ -61,18 +66,22 @@ class WeaklyCoupledModel:
             print("Solved for t = ", self.t.value)
 
     def transfer_Ta(self):
-        # Now I am saving T_a to a function defined on the EP pde space, 
+        # Now I am saving T_a to a function defined on the EP pde space,
         # and then interpolating it into the T_a space (DG, 0).
-        #? Is this okay? Maybe problems when using different meshes?
-        Ta_array = self.ep.ode.model.monitor_values(self.t.value, 
-                                              self.ep.ode.states, 
-                                              self.ep.ode.params)[self.Ta_index]
+        Ta_array = self.ep.ode.model.monitor_values(
+            self.t.value, self.ep.ode.states, self.ep.ode.params
+        )[self.Ta_index]
         self.Ta_lagrange.x.array[:] = Ta_array
         self.mech.set_tension(self.Ta_lagrange)
 
-    def solve(self, T, N = 10, save_displacement = False):
-        if save_displacement: 
-            vtx = io.VTXWriter(MPI.COMM_WORLD, "coupling1way.bp", [self.mech.u, self.ep.pde.vn], engine="BP4")
+    def solve(self, T, N=10, save_displacement=False):
+        if save_displacement:
+            vtx = io.VTXWriter(
+                MPI.COMM_WORLD,
+                "coupling1way.bp",
+                [self.mech.u, self.ep.pde.vn],
+                engine="BP4",
+            )
         n = 0
         while self.t.value < T + self.dt:
             n += 1
@@ -86,6 +95,3 @@ class WeaklyCoupledModel:
                 vtx.write(self.t.value)
         if save_displacement:
             vtx.close()
-
-
-
